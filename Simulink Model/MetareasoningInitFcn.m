93d972fb705d681646abd4ff85162e88f0ee8209
Aidan Wallace <64740751+aidnaw@users.noreply.github.com>
2021-05-15 11:44:46 -0400

    Update Documentation

--- Simulink Model/MetareasoningInitFcn.m
+++ Simulink Model/MetareasoningInitFcn.m
@@ -1,11 +1,11 @@
 %% Initial conditions for CD&R simulation 
 
-num_ac = 2;
-pos_ic = [0,0,0; 22224, 23150,0];
-vel_ic = [205.7783, 246.9339];
-chi_ic = deg2rad([0, 270]);
-gamma_ic = zeros(1,num_ac);
-goal_pos = [9000, 1E6, 0; -1E6, 23150+9000, 0];
+num_ac = 2; %Number of aircraft
+pos_ic = [0,0,0; 22224, 23150,0]; %Initial positions [m]
+vel_ic = [205.7783, 246.9339]; %Initial velocities [m/s]
+chi_ic = deg2rad([0, 270]); %Initial headings BASED OFF EAST [rad]
+gamma_ic = zeros(1,num_ac); %Initial flight path angles [rad]
+goal_pos = [9000, 1E6, 0; -1E6, 23150+9000, 0]; %Goal positions (Frazzoli 2005)
 
 x0 = pos_ic(:,1).';
 y0 = pos_ic(:,2).';
